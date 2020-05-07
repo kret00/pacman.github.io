60a25022223605298cc5af842651ee3304f16b31
kret00 <64218815+kret00@users.noreply.github.com>
2020-05-07 18:22:47 +0200

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,5 +1,5 @@
   var setCookie = function (n, val) {
-  var exdays = 365;
+  var exdays = 5;
   var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   var expires = "expires=" + d.toGMTString();
@@ -35,7 +35,7 @@ document.onclick = function (e) {
   
   //Change value to experiment with amount of cookies
   if (e.target.id === 'multiple-cookies') {
-        generateBigMultipleCookies(10,4045)
+        generateBigMultipleCookies(100,4041)
   }
 };
 
