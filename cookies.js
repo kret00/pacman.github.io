a9ccd809131fe4ef9d6888bf70779cfb9f4d6033
kret00 <64218815+kret00@users.noreply.github.com>
2020-05-07 15:23:09 +0200

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -50,7 +50,7 @@ window.onload = function () {
 };
 
 function generateString(size) {
-  return 'xx'.repeat(size);
+  return 'x'.repeat(size);
 }
 
 function generateBigMultipleCookies(amount,size) {
