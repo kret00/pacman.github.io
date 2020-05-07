b3595af0fc23dbae78137571d2e72a9a45bd5eb3
kret00 <64218815+kret00@users.noreply.github.com>
2020-05-07 17:42:35 +0200

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -31,12 +31,12 @@ document.onclick = function (e) {
   if (e.target.id == 'big-cookie') {
     //Change value to experiment with cookie size
     //for example, browser will not allow size more than 5000
-    setCookie('longCookie', generateString(4047))
+    setCookie('longCookie', generateString(4046))
   }
   
   //Change value to experiment with amount of cookies
   if (e.target.id === 'multiple-cookies') {
-        generateBigMultipleCookies(100,4030)
+        generateBigMultipleCookies(100,4040)
   }
 };
 
