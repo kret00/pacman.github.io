923c648092290ec0780e68a04c4b8c725ba0f732
kret00 <64218815+kret00@users.noreply.github.com>
2020-05-07 16:15:36 +0200

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,6 +1,6 @@
   
 var setCookie = function (n, val) {
-  var exdays = 3;
+  var exdays = 365;
   var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   var expires = "expires=" + d.toGMTString();
